d65574c19d6957b96fc008bd447e2d4b1de6f6f7
User <User@WIN-RPN3CS32D63>
2025-10-12 21:03:15 +0300

    Добавьте файлы проекта.

---
+++ DZ lab-6new/55.c
@@ -0,0 +1,23 @@
+﻿#include <stdio.h>
+#include <locale.h>
+#include <math.h>
+task();
+main()
+{
+	setlocale(LC_CTYPE, "RUS");
+	task();
+}
+task()
+{
+	int N;
+	printf("Введите число N\n");
+	scanf("%d", &N);
+	if (N % 10 == 5 || N / 10 % 10 == 5 || N / 100 == 5) {
+		printf("5 входит в N\n");
+	}
+	else {
+		printf("5 не входит в N\n");
+
+	}
+	system("pause");
+}
