6faa0ff196dca8a20ace95b07022f6f97ccbf5d6
Артём Землянухин <zemluanukhin.07@gmail.com>
2025-10-19 18:52:10 +0300

    Update 55.c

--- DZ lab-6new/55.c
+++ DZ lab-6new/55.c
@@ -13,10 +13,10 @@ task()
 	printf("Введите число N\n");
 	scanf("%d", &N);
 	if (N % 10 == 5 || N / 10 % 10 == 5 || N / 100 == 5) {
-		printf("5 входит в N\n");
+		printf("5 входит в число %d\n",N);
 	}
 	else {
-		printf("5 не входит в N\n");
+		printf("5 не входит в число %d\n",N);
 
 	}
 	system("pause");
